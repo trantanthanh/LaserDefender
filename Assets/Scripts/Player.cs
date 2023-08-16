005310227c88b8370a4063a1b236fc7a12bbc35d
Thành Trần Tấn <trantanthanh@gmail.com>
2023-08-16 23:29:17 +0700

    clamp ship moving with camera view port

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 rawInput;
+
+    Vector2 minBounds;//(0, 0)
+    Vector2 maxBounds;//(1, 1)
+
+    void Start()
+    {
+        InitBounds();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+        // Debug.Log(minBounds);
+        // Debug.Log(maxBounds);
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        // Debug.Log(rawInput);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 deltaMove = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + deltaMove.x, minBounds.x, maxBounds.x);
+        newPos.y = Mathf.Clamp(transform.position.y + deltaMove.y, minBounds.y, maxBounds.y);
+        transform.position = newPos;
+    }
+}
